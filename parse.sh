700840179b9eed1302d91166c25eb5c3dfe679dd
Peter Love <p.love@lancaster.ac.uk>
2016-04-05 10:52:14 +0200

    parse htcondor history files

--- parse.sh
+++ parse.sh
@@ -5,7 +5,8 @@ shopt -s nullglob
 for f in /var/lib/condor/spool/history.*; do
   bn=$(basename $f)
   p=${bn/history/parsed}
-  echo /data/plove/$p
-  if [ -f $p ]; then continue; fi
-  condor_history -l -file $f > /data/plove/$p
+  fullp=/data/plove/$p
+  echo $fullp
+  if [ -f $fullp ]; then continue; fi
+  condor_history -l -file $f > $fullp
 done
