5ada8f7ba3d6798f6929225ce49e6d62cd2aca9f
Peter Love <p.love@lancaster.ac.uk>
2016-08-03 16:03:14 +0200

    have clean queues.d

--- all-agis24.sh
+++ all-agis24.sh
@@ -2,6 +2,10 @@
 
 cmd=/usr/local/bin/agis.py
 
+# only use defined clouds
+rm -f /etc/autopyfactory/queues.d/*-analy.conf
+rm -f /etc/autopyfactory/queues.d/*-prod.conf
+
 for c in "$@"; do
   dest=/etc/autopyfactory/queues.d/$c-analy.conf
   echo $dest
