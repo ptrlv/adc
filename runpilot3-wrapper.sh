c2c11a6bdd73c0a6efef9eeaae19d2cc7da288b1
Peter Love <p.love@lancaster.ac.uk>
2019-04-01 11:20:57 +0200

    bump

--- runpilot3-wrapper.sh
+++ runpilot3-wrapper.sh
@@ -6,7 +6,7 @@
 
 # https://google.github.io/styleguide/shell.xml
 
-VERSION=20190304a
+VERSION=20190401a
 
 echo "This is ATLAS pilot wrapper version: $VERSION"
 echo "Please send development requests to p.love@lancaster.ac.uk"
